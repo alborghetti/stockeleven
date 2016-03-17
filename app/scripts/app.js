2cf2d54048cc14b3e0631623bf5b02078849c376
Francesco Alborghetti <francesco.alborghetti@datavard.com>
2016-03-18 00:01:33 +0100

    Initial commit with contributors

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name stockElevenApp
+ * @description
+ * # stockElevenApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('stockElevenApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name stockElevenApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the stockElevenApp
+ */
+angular.module('stockElevenApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
