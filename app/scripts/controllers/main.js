af2a37247cdc69c030f0cd5e30f3416dbc5b034b
Francesco Alborghetti <f.alborghetti@gmail.com>
2016-05-01 23:47:16 +0200

    ng-hide

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,7 +15,7 @@ angular.module('stockElevenApp')
   	});
 
   	$rootScope.$emit('viewLoaded');
-
+ 
   	$scope.isActive = function(route) {
         return route === $location.path();
     }
@@ -31,20 +31,20 @@ angular.module('stockElevenApp')
 	var ref = new Firebase("https://stockeleven.firebaseio.com/");
 	ref.onAuth(function(authData) {
 		if (authData) {
-			$scope.isLoggedIn = true;
 			ref = new Firebase("https://stockeleven.firebaseio.com/users/"+authData.uid);
 			ref.once("value", function(snapshot) {
       			var snapshotUser = snapshot.val();
 
 				$scope.$apply(function() {
 					$scope.userName = snapshotUser.firstName + ' ' + snapshotUser.lastName;
+					$scope.isLoggedIn = true;
 				});
 				}, function (errorObject) {
 					console.log("The read failed: " + errorObject.code);
 			});
 
 		} else {
-			$scope.isLoggedIn = false;
+				$scope.isLoggedIn = false;
 		}
 	});
 
